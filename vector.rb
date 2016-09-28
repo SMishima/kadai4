2cd60fcd1d92526cd0a07a80b5e588cbbbfd6e13
d1234 <d1234>
2016-09-28 12:05:09 +0900

    second upload

--- vector.rb
+++ vector.rb
@@ -11,12 +11,28 @@ class Vector
   def length
     return Math.sqrt(@x**2 + @y**2)
   end
+
+  def x
+    return @x
+  end
+
+  def y
+    @y
+  end
+
+  def add(v)
+    x3 = @x + v.x
+    y3 = @y + v.y
+    return "(#{x3},#{y3})"
+  end
 end
 
 a = Vector.new(1,2)
+b = Vector.new(2,3)
 
+puts a.to_s
 
-s = a.to_s
-puts s
-l = a.length
-puts l
+puts a.length
+
+c = a.add(b)
+puts c
