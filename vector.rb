697bb33a1be3eac6d6c365609edabf4d974e436f
d1234 <d1234>
2016-09-28 13:07:06 +0900

    teisei

--- vector.rb
+++ vector.rb
@@ -1,38 +1,44 @@
 class Vector
+  #初期値をセット
   def initialize(x,y)
     @x = x
     @y = y
   end
 
+  #ベクトルを表示
   def to_s
     return "(#{@x},#{@y})"
   end
 
+  #ベクトルの長さを計算
   def length
     return Math.sqrt(@x**2 + @y**2)
   end
 
+  #x座標をセット
   def x
     return @x
   end
 
+  #y座標をセット
   def y
     @y
   end
 
+  #ベクトルの和を計算
   def add(v)
     x3 = @x + v.x
     y3 = @y + v.y
-    return "(#{x3},#{y3})"
+    return Vector.new(x3,y3)
   end
 end
 
-a = Vector.new(1,2)
-b = Vector.new(2,3)
+a = Vector.new(1,2)	#a = (1, 2)
+b = Vector.new(2,3)	#b = (2, 3)
 
 puts a.to_s
 
 puts a.length
 
-c = a.add(b)
+c = a.add(b)	#c = a + b = (3, 5)
 puts c
